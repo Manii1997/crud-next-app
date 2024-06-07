06e28048f7b26daf316eb25a70e0aa598caeb15e
Manii1997 <manibct1817@gmail.com>
2024-06-07 10:58:36 +0530

    Updated popups

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
 import { IoMdSearch } from "react-icons/io";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
   const [taskList, setTaskList] = useState([]);
@@ -15,9 +17,10 @@ const Home = () => {
   }, []);
 
   const addNewTask = (task) => {
-    const updatedTasks = [...taskList, task];
+    const updatedTasks = [task, ...taskList];
     setTaskList(updatedTasks);
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    toast.success("Task added successfully!");
   };
 
   const updateTask = (updatedTask) => {
@@ -26,12 +29,14 @@ const Home = () => {
     );
     setTaskList(updatedTasks);
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    toast.info("Task updated successfully!");
   };
 
   const deleteTask = (id) => {
     const filteredTaskList = taskList.filter((task) => task.id !== id);
     setTaskList(filteredTaskList);
     localStorage.setItem("tasks", JSON.stringify(filteredTaskList));
+    toast.error("Task deleted successfully!");
   };
 
   const filteredTasks = taskList.filter((task) =>
@@ -40,11 +45,12 @@ const Home = () => {
 
   return (
     <div className="main-container container mx-auto p-3">
+      <ToastContainer position="top-center" />
       <h1 className="heading text-2xl font-bold text-center">
         CRUD APPLICATION
       </h1>
       <div className="flex flex-col md:flex-row gap-3 md:justify-between">
-        <div className="">
+        <div>
           <AddTask addTask={addNewTask} />
         </div>
 
@@ -59,16 +65,20 @@ const Home = () => {
           <IoMdSearch />
         </div>
       </div>
-      <ul className="md:grid md:grid-cols-3 gap-3 mt-10">
-        {filteredTasks.map((taskItem) => (
-          <TodoList
-            key={taskItem.id}
-            taskDetails={taskItem}
-            deleteTask={deleteTask}
-            updateTask={updateTask}
-          />
-        ))}
-      </ul>
+      {filteredTasks.length > 0 ? (
+        <ul className="md:grid md:grid-cols-3 gap-3 mt-10">
+          {filteredTasks.map((taskItem) => (
+            <TodoList
+              key={taskItem.id}
+              taskDetails={taskItem}
+              deleteTask={deleteTask}
+              updateTask={updateTask}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className="text-center mt-10">No tasks found</p>
+      )}
     </div>
   );
 };
